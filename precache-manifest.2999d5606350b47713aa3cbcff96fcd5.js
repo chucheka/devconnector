a5556f188a770c23bd671ea4cdecfa270912be9a
ucheka22 <ryanucheka@gmail.com>
2019-11-08 08:13:29 +0100

    Updates

---
+++ precache-manifest.2999d5606350b47713aa3cbcff96fcd5.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3da5c666d3b99bde9c1a98a1df32635a",
+    "url": "/devconnector/index.html"
+  },
+  {
+    "revision": "d9dc371ef1b3aa5112f9",
+    "url": "/devconnector/static/css/main.dd2b400f.chunk.css"
+  },
+  {
+    "revision": "879a2912f8a1588cccde",
+    "url": "/devconnector/static/js/2.53d76357.chunk.js"
+  },
+  {
+    "revision": "d9dc371ef1b3aa5112f9",
+    "url": "/devconnector/static/js/main.eb8d0f93.chunk.js"
+  },
+  {
+    "revision": "2b90e196c9a8e87ab263",
+    "url": "/devconnector/static/js/runtime-main.09d71496.js"
+  },
+  {
+    "revision": "892b3f33789a99517849e9373b4761b6",
+    "url": "/devconnector/static/media/facebook.892b3f33.png"
+  },
+  {
+    "revision": "9c62667769a965e4e593f890ed4b65d3",
+    "url": "/devconnector/static/media/instagram.9c626677.png"
+  },
+  {
+    "revision": "9a513524f2be4401637c4fee6191cd2e",
+    "url": "/devconnector/static/media/twitter.9a513524.png"
+  }
+]);
